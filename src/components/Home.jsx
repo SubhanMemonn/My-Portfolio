fdff5f781afbbb68caac37f0f17e190553be5e52
subhan-saleem7 <subhanmemon131@gmail.com>
2024-02-23 22:13:20 +0500

    Add Img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { BiLogoGmail } from "react-icons/bi";
 const Home = () => {
   return (
     <>
-      <div className="w-full max-w-3xl mx-auto flex flex-col sm:flex-row md:items-center p-3 sm:p-2 gap-8 mt-10">
+      <div className="w-full max-w-3xl mx-auto flex flex-col sm:flex-row sm::items-center p-3 sm:p-2 gap-8 mt-10">
         <div className="flex flex-col gap-6">
           <h1 className=" font-bold text-3xl ">MERN Stack Developer 👋</h1>
           <p>
@@ -29,35 +29,13 @@ const Home = () => {
             </a>
           </div>
         </div>
-
-        <svg
-          viewBox="0 0 500 500"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          width="80vw"
-          id="blobSvg"
-        >
-          <defs>
-            <clipPath id="shape">
-              <path
-                id="blob"
-                d="M455,338Q426,426,338,453Q250,480,163,452Q76,424,41.5,337Q7,250,42,163.5Q77,77,163.5,41Q250,5,335,42.5Q420,80,452,165Q484,250,455,338Z"
-                fill="none"
-                strokeWidth="7px"
-                stroke="#d1d8e0"
-              ></path>
-            </clipPath>
-          </defs>
-          <image
-            x="0"
-            y="0"
-            width="100%"
-            height="100%"
-            clip-path="url(#shape)"
-            xlinkHref="https://res-console.cloudinary.com/dommefrmd/media_explorer_thumbnails/e45878ee1b2eda384912c420bd034605/detailed"
-            preserveAspectRatio="none"
-          ></image>
-        </svg>
+        <div className="rounded-[50%] overflow-hidden w-full h-56">
+          <img
+            src="./PSX_20240223_010423.jpg"
+            alt="me"
+            className="w-full h-full object-cover rounded-full"
+          />
+        </div>
       </div>
 
       <div className=" flex mt-11 md:items-center p-3 sm:p-2 w-full max-w-3xl mx-auto flex-col sm:flex-row gap-3">
